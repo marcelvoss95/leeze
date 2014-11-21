788acb0613ac29e165ed758110f841bf16b22dc8
marcelvoss95 <marcel.voss@web.de>
2014-11-21 08:00:25 -0800

    korrektur

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :tours
 
-  root :to => 'toursController#index'
+  root :to => 'ToursController#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
